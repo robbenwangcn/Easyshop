cc56496e3e99060c5b1e4ef5e435f84cf935bdb3
Robben <15948339607@163.com>
2018-07-31 09:05:49 +0800

    Init Easyshop

---
+++ WebService/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  DB: 'mongodb://localhost:27017/easyshop',
+  APP_PORT: 3000
+}
---
+++ public/bus.js
@@ -0,0 +1,8 @@
+//path/to/project/public/bus.js
+'use strict'
+
+import Vue from 'vue'
+
+const bus = new Vue()
+
+export default bus
